b319ece6aecb5d20d397fd37fcb0bfecefa7a469
unknown <johirul35-257@diu.edu.bd>
2022-12-01 00:13:18 +0600

    Category model created

---
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+
+    public function index()
+    {
+        return "hello";
+    }
+
+
+    public function create()
+    {
+        //
+    }
+
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+
+    public function show(Category $category)
+    {
+        //
+    }
+
+
+    public function edit(Category $category)
+    {
+        //
+    }
+
+
+    public function update(Request $request, Category $category)
+    {
+        //
+    }
+
+
+    public function destroy(Category $category)
+    {
+        //
+    }
+}
